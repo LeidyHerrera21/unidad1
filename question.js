197faf3d807d2a394b1550d0d85e66c824d03ef8
Leidy Herrera <152449621+LeidyHerrera21@users.noreply.github.com>
2025-02-14 09:55:29 -0500

    Create question.js

---
+++ question.js
@@ -0,0 +1,58 @@
+let questions =
+
+  numb, 1,
+    questions; "¿Cual es el mamifero mas grande?",
+      answer; "B. Ballena azul",
+        options;[
+          "A. Jirafa",
+          "B. Ballena azul",
+          "C. Elefante",
+          "D. Hipopotamo"
+        ]
+
+{
+  numb: 2,
+    question; "¿Cual es el mamifero que pone huevos?",
+      answer; "C. Ornitorrinco",
+        options;[
+          "A. Pato",
+          "B. Castor",
+          "C. Ornitorrinco",
+          "D. Nutria",
+        ]
+}
+{
+  numb: 3,
+    question; "¿Cual es el animal mas pequeño?",
+      answer; "A. Tardígrado  ",
+        options;[
+          "A. Tardígrada",
+          "B.  Mosca",
+          "C.  Rana",
+          "D.  Abeja",
+        ]
+
+}
+{
+  numb: 4,
+    question; "¿Cuál es el animal más rápido del mundo?",
+      answer; "D. Guepardo ",
+        Option;[
+          "A. Tiburón",
+          "B. Tigre",
+          "C, Aguila",
+          "D. Guepardo",
+        ]
+}
+
+{
+  numb: 5,
+    question; "¿Cuál es el animal más alto del mundo?",
+      answer; "B. Jirafa  ",
+        Option;[
+          "A.  Rinoceronte",
+          "B.  Elefante",
+          "C,  Ardilla",
+          "D.  Jirafa",
+        ]
+}
